e56073338a60eeeba2f2d25ea68536909fd70c72
Adrien Abbey <abbey.12@wright.edu>
2025-12-19 22:20:22 -0500

    implemented rotate function, finished main

--- day1/day1.py
+++ day1/day1.py
@@ -2,17 +2,32 @@
 """
 AoC 2025 Day 1 in Python
 
+https://adventofcode.com/2025/day/1
+
 Author: Adrien Abbey
 Date: 2025-12-19
 """
 
+# =====================================
+# Imports
+# =====================================
 
 from pathlib import Path
 
 
-INPUT_FILE_PATH = "day1input.txt"
-TESTING = True
+# =====================================
+# Constants and Globals
+# =====================================
 
+INPUT_FILE_PATH = "day1input.txt"
+TESTING = False
+location = 50   # Tracks dial location, starting at 50
+total_count = 0  # Track the number of times a rotation ended on zero
+
+
+# =====================================
+# Helper Functions
+# =====================================
 
 def load_input(path: str | Path) -> list[str]:
     """
@@ -45,16 +60,65 @@ def parse_input_string(input_string: str) -> tuple[str, int]:
     return direction, count
 
 
+def rotate(direction: str, count: int) -> None:
+    """
+    Rotates the dial in the direction given the number of steps given.
+
+    :param direction: Either "L" or "R"
+    :type direction: str
+    :param count: The number of steps to rotate
+    :type count: int
+    """
+    global location  # Make the location variable editable
+
+    # Rotate the dial in the correct direction and number of times:
+    if direction == "L":
+        location -= count
+    elif direction == "R":
+        location += count
+    else:
+        raise ValueError(f"Invalid rotation direction: {direction!r}")
+
+    # Constrain the location to valid locations:
+    while location > 99 or location < 0:
+        if location > 99:
+            location -= 100
+        if location < 0:
+            location += 100
+
+
+# =====================================
+# Main
+# =====================================
+
 def main() -> None:
+    """
+    The main function is the main function.  It does all the important stuff.
+    """
+    global total_count  # Make the total_count global variable editable
+
     input_file_contents = load_input(INPUT_FILE_PATH)  # Load the input file
 
     # Parse the input into a usable format:
     parsed_inputs = [parse_input_string(line) for line in input_file_contents]
 
+    # Testing function to verify my code works:
     if TESTING:
         print(f"Loaded {len(input_file_contents)} lines.")
         print(f"Parsed input example: {parsed_inputs[0:3]}")
 
+    # Follow the rotation instructions:
+    for rotation in parsed_inputs:
+        rotate(rotation[0], rotation[1])
+        if location == 0:
+            total_count += 1    # Update the total number of times the rotation stops at zero
+
+    # Print out the results:
+    print(f"The dial stopped on zero {total_count} times.")
+
 
 if __name__ == "__main__":
+    """
+    This will ensure the main function gets called when the file gets executed.
+    """
     main()
